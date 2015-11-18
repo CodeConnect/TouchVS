c36f960cdf48602d8ce9563eb8bfa570f6c667b7
Amadeusz Wieczorek <git@amadeusw.com>
2015-11-18 07:35:59 -0800

    Adds test project

---
+++ test/CodeConnect.Touch.Tests/RenderingTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using CodeConnect.Touch.Helpers;
+
+namespace CodeConnect.Touch.Tests
+{
+    public class RenderingTests
+    {
+        [Fact]
+        public void CorrectPathDataCreatedForSegments()
+        {
+            var s1 = TouchControlShapeFactory.GetSegment(5, 0);
+            var s2= TouchControlShapeFactory.GetSegment(5, 1);
+            var s3= TouchControlShapeFactory.GetSegment(5, 2);
+            var s4= TouchControlShapeFactory.GetSegment(5, 3);
+            var s5 = TouchControlShapeFactory.GetSegment(5, 4);
+        }
+    }
+}
